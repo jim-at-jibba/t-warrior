f88d442917968d96343a3f86ee4637ef1869035c
James Best <james.best@breedr.co>
2021-08-23 20:57:38 +0100

    Add typescript paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@Components": "./src/components",
+            "@Utils": "./src/utils",
+            "@Hooks": "./src/hooks",
+            "@Types": "./src/types",
+            "@Constants": "./src/constants",
+            "@Store": "./src/store",
+            "@Features": "./src/features",
+            "@Nav": "./src/navigation",
+          },
+        },
+      ],
+    ],
+  }
+}
